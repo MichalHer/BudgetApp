f4141c1b4acf1023097c9fd79ec6f2a45885e033
Michał Herczyński <mherczynski1991@gmail.com>
2022-10-14 13:13:17 +0200

    Budget Chart

--- frontend/website/static/js/dashboard.js
+++ frontend/website/static/js/dashboard.js
@@ -1,7 +1,8 @@
 import {get_operations} from "./operations_api.js";
 import {get_predictions} from "./predictions_api.js";
 import {get_transfers} from "./transfers_api.js";
-import {get_accounts} from "./accounts_api.js"
+import {get_accounts} from "./accounts_api.js";
+import "https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js";
 
 async function get_currency_from_nbp(code){
     const response = await fetch(`http://api.nbp.pl/api/exchangerates/rates/A/${code}/`, {method:'GET', 'Content-Type':'application/json'});
@@ -10,7 +11,6 @@ async function get_currency_from_nbp(code){
 }
 
 async function get_accounts_summary() {
-    var sum = 0;
     var table = "<thead>\
                     <tr>\
                             <td>Konto</td>\
@@ -20,12 +20,6 @@ async function get_accounts_summary() {
                 <tbody>"
     for(const account of accounts) {
         var operations_value = 0;
-        var predictions_value = 0;
-        // predictions.forEach(prediction => {
-        //     if (prediction.account == account.ID_Acc){
-        //         predictions_value += prediction.value;
-        //     }
-        // });
         operations.forEach(operation => {
             if (operation.account == account.ID_Acc){
                 operations_value += operation.value;
@@ -48,11 +42,11 @@ async function get_accounts_summary() {
             const currency_data = await get_currency_from_nbp(account.currency);
             currency_value = currency_data.rates[0].mid;
         }
-        sum += operations_value * currency_value;
+        current_accounts_value += operations_value * currency_value;
     }
     table += `<tr>
                 <td>SUMA</td>
-                <td>${sum.toFixed(2)} PLN</td>
+                <td>${current_accounts_value.toFixed(2)} PLN</td>
             </tr>`
     table += "</tbody>";
     document.getElementById("dashboard_accounts_table").innerHTML = table;
@@ -60,45 +54,45 @@ async function get_accounts_summary() {
 
 function current_month_summary(predictions) {
     var table = '';
-for(const prediction of predictions){
-    var operations_value = 0;
-    var currency = accounts.find(x => x.ID_Acc == prediction.account).currency
-    operations.forEach(element => {
-        if (element.prediction == prediction.ID_Pred){
-            operations_value += element.value;
+    for(const prediction of predictions){
+        var operations_value = 0;
+        var currency = accounts.find(x => x.ID_Acc == prediction.account).currency
+        operations.forEach(element => {
+            if (element.prediction == prediction.ID_Pred){
+                operations_value += element.value;
+            }
+        });
+        var balance = ((prediction.value - operations_value)*-1).toFixed(2)
+        var color = null;
+        if(prediction.value < 0){
+            if (balance == 0){
+                color = "table-success";
+            } else if (balance < 0){
+                color = "table-danger";
+            }
+        } else {
+            if (balance < 0){
+                color = "table-danger";
+            } else if (balance >= 0){
+                color = "table-success";
+            }
         }
-    });
-    var balance = ((prediction.value - operations_value)*-1).toFixed(2)
-    var color = null;
-    if(prediction.value < 0){
-        if (balance == 0){
-            color = "table-success";
-        } else if (balance < 0){
-            color = "table-danger";
-        }
-    } else {
-        if (balance < 0){
-            color = "table-danger";
-        } else if (balance >= 0){
-            color = "table-success";
-        }
-    }
 
-    table += `<tr`;
-    if (color != null){
-        table += ` class="${color}"`;
+        table += `<tr`;
+        if (color != null){
+            table += ` class="${color}"`;
+        }
+        table += `>\
+                    <td>${prediction.date}</td>
+                    <td>${accounts.find(x => x.ID_Acc == prediction.account).name}</td>
+                    <td>${prediction.purpose_of_the_expendture}</td>
+                    <td>${prediction.value.toFixed(2)} ${currency}</td>
+                    <td>${operations_value.toFixed(2)} ${currency}</td>
+                    <td>${balance} ${currency}</td>
+                </tr>`
     }
-    table += `>\
-                <td>${prediction.date}</td>
-                <td>${accounts.find(x => x.ID_Acc == prediction.account).name}</td>
-                <td>${prediction.purpose_of_the_expendture}</td>
-                <td>${prediction.value.toFixed(2)} ${currency}</td>
-                <td>${operations_value.toFixed(2)} ${currency}</td>
-                <td>${balance} ${currency}</td>
-            </tr>`
-}
-document.getElementById("dashboard_current_month_body").innerHTML = table;
-document.getElementById("filter_btn").addEventListener("click", current_month_summary_filter,true);
+    document.getElementById("dashboard_current_month_body").innerHTML = table;
+    document.getElementById("filter_btn").addEventListener("click", current_month_summary_filter,true);
 }
 
 async function current_month_summary_filter(){
@@ -123,6 +117,40 @@ function default_date_range(){
     return [date_from, date_to];
 }
 
+function get_twelve_months(){
+    var result = [];
+    for(let x = 0; x < 13; x++){
+        let date = new Date();
+        date.setMonth(date.getMonth()+(x));
+        let year = date.getFullYear();
+        let month = date.getMonth();
+        result.push(`${year}-${month+1}`); 
+    }
+    return result
+}
+
+function get_twelve_months_budget(dates){
+    var result = [current_accounts_value]
+    var previous_sum = current_accounts_value;
+    for (let x = 1; x < dates.length; x++){
+        let date = dates[x];
+        let date_array = date.split("-");
+        let date_from = Date.parse(date+"-1");
+        let date_to = Date.parse(date + `-${last_day_of_month(date_array[0], date_array[1])}`);
+        let temp_predictions = predictions.filter(x => Date.parse(x.date) >= date_from && Date.parse(x.date) <= date_to);
+        var tmp_predictions_sum = 0;
+        for (const prediction of temp_predictions){
+            tmp_predictions_sum += prediction.value;
+        }
+        result.push(tmp_predictions_sum+previous_sum);
+        previous_sum = tmp_predictions_sum+previous_sum;
+    }
+    console.log(result);
+    return result;
+}
+
+
+
 let date_range = default_date_range();
 document.getElementById("date_from").value = date_range[0];
 document.getElementById("date_to").value = date_range[1];
@@ -132,9 +160,28 @@ const transfers = await get_transfers(user);
 const predictions = await get_predictions(user);
 const operations = await get_operations(user);
 const accounts = await get_accounts(user);
-
-window.onload = get_accounts_summary();
+var current_accounts_value = 0;
 window.onload = current_month_summary_filter();
+window.onload = await get_accounts_summary();
+
+
+
+var dates = get_twelve_months();
+var values = get_twelve_months_budget(dates);
+
+
+window.onload = new Chart("chartContainer", {
+    type: "bar",
+    data: {
+        labels: dates,
+        datasets: [{
+            data: values
+        }]
+    },
+    options:{legend: {display: false}}
+});
+
+
 
 document.getElementById("filter_btn").addEventListener("click", current_month_summary_filter,true);
 
